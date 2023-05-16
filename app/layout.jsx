5ea6eb8048185f3a6c4ce8d4d84335bc51ce4807
Kusson <kussonlks@gmail.com>
2023-05-16 22:14:57 +1000

    Basic setup of app; fonts and styling

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import Head from 'next/head';
 import "@styles/globals.css";
+import Navbar from '@components/navbar';
+import logo from "@public/images/hushlogo_bg.png";
+
 
 //metadata for the entire website
 export const metadata = {
     title: "Hush Hush Mechanic - Quietly Brilliant",
-    description: "Your one-stop-shop for all roadie needs"
+    description: "Your one-stop-shop for all roadie needs",
+    icon: {logo},
 }
 
 const RootLayout = ( {children} ) => {
   return (
     <html>
+        <head>
+            {/* importing Google Fonts */}
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link href="https://fonts.googleapis.com/css2?family=Lilita+One&family=Roboto+Flex:wght@100;200;300;400;500;600;700;800;900;1000&display=swap" rel="stylesheet" />
+        </head>
+        
         <body>
+        <Navbar />
             <main className="app">
                 {children}
             </main>
--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className= "container">
+            <div className="homeTitle">
+                <h1>Hush Hush Mechanic</h1>
+                <p>Your one-stop-shop for all roady needs</p>
+            </div>
+        
+    </div>
   )
 }
 
