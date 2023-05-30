a26260cf0cd4f859ac32c3488f9d5a61f4563c7c
Kusson <kussonlks@gmail.com>
2023-05-31 07:35:04 +0800

    Basic form structure + selection complete

--- app/booking/page.jsx
+++ app/booking/page.jsx
@@ -6,15 +6,35 @@ import "@styles/booking.css";
 const Booking = () => {
   return (
     <div className="bookingContainer">
-    <h1>Book your service today</h1>
-    <form>
-      <h2>Name:</h2>
-      <input></input>
-      <h2>Number:</h2>
-      <input></input>
-      <h2>email:</h2>
-      <input></input>
-    </form>
+      <h1>Book your service today</h1>
+
+      <form className="bookingForm">
+        <div className="formField">
+          <label>Name: </label>
+          <input></input>
+        </div>
+
+        <div className="formField">
+          <label>Number: </label>
+          <input></input>
+        </div>
+
+        <div className="formField">
+          <label>email: </label>
+          <input></input>
+        </div>
+
+        <label for="serviceSelection">Choose a service: </label>
+        <select name="serviceSelection" id="serviceSelection">
+          <option value="thecaress">The Caress</option>
+          <option value="themeticulous">The Meticulous</option>
+          <option value="theoverhaul">The Overhaul</option>
+          <option disabled="true">--------------</option>
+          <option>Wheel Building</option>
+        </select> 
+
+      </form>
+
     </div>
   )
 }
